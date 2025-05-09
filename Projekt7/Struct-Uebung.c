8c3eefc57194886f8b928778618f3b7f9bf60b07
RaxoTheOne <traxon.bg@gmail.com>
2025-05-09 11:15:51 +0200

    Struct Uebung created with exercise

---
+++ Projekt7/Struct-Uebung.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Person{
+    char name[100];
+    int alter;
+    float groesse;
+};
+
+int main(){
+    struct Person personen[3]; // Platz für 3 Personen
+    //Eingabe der Daten
+    printf("\n----Personenliste----\n");
+    printf("Geben Sie die Daten der Personen ein:\n");
+    // Schleife für die Eingabe der Daten
+    for(int i = 0; i < 3; i++){
+        printf("Geben Sie den Namen der Person %d ein: ", i + 1);
+        scanf("%s", personen[i].name);
+        printf("Geben Sie das Alter der Person %d ein: ", i + 1);
+        scanf("%d", &personen[i].alter);
+        printf("Geben Sie die Groesse der Person %d ein: ", i + 1);
+        scanf("%f", &personen[i].groesse);
+    }
+    //Ausgabe der Daten
+    printf("\n----Personenliste----\n");
+    for(int i = 0; i < 3; i++){
+        printf("Name: %s, Alter: %d, Groesse: %.2f\n", personen[i].name, personen[i].alter, personen[i].groesse);
+    }
+
+}
